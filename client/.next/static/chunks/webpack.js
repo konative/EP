cbda13c2b3f2b90776de760b047870a246f1f0a2
Karthik Gurram <kggurram@gmail.com>
2022-05-28 00:31:32 -0400

    gitignore fix

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -128,7 +128,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "21368947fb090357"; }
+/******/ 		__webpack_require__.h = function() { return "8dcd292904a537e0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
